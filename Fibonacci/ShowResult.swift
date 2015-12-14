3f3f0bedf54af9942e800b12103f42b8a5aa43da
Amy Chow <csamyphew.cky@gmail.com>
2015-12-14 12:25:21 +0800

    Add view controller to each view. Link up the elements to the controller file.

--- Fibonacci/ShowResult.swift
+++ Fibonacci/ShowResult.swift
@@ -1,30 +1,25 @@
 //
-//  ViewController.swift
+//  ShowResult.swift
 //  Fibonacci
 //
-//  Created by Amy Chow on 12/12/2015.
+//  Created by Amy Chow on 14/12/2015.
 //  Copyright © 2015 Amy Chow. All rights reserved.
 //
 
 import UIKit
 
-class ViewController: UIViewController {
-
-    @IBOutlet weak var input: UITextField!
+class ShowResult: UIViewController {
+    
+    @IBOutlet weak var userInput: UILabel!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
 
     
-    @IBAction func enter() {
-    }
-    
-
 }
-
